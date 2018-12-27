ffe99d40a9048336880010568adb82cdf3d1a53f
SteliosKats <stelios.katsiadramis@gmail.com>
2018-12-27 04:37:01 +0200

    Updates on Sets

--- src/main/scala/homegrown/collections/Set.scala
+++ src/main/scala/homegrown/collections/Set.scala
@@ -1,13 +1,19 @@
 package homegrown.collections
 
 trait Set extends (String => Boolean) {
-  final def add(input: String): Set = element => element == input || this(element)
-  final def remove(input: String): Set = element => input != element || !this(element)
+  def add(input: String): Set = element => element == input || this(element)
+  final def remove(input: String): Set = element => input != element && !this(element)
   final def union(that: Set): Set = element => this(element) || that(element)
   final def intersection(that: Set): Set = element => this(element) && that(element)
   final def diff(that: Set): Set = element => this(element) && !that(element)
 }
 
 object Set {
-  def empty: Set = input => false
+  private final case class NonEmpty(element: String, otherElements: Set) extends Set {
+    final override def apply(input: String): Boolean = input == element || otherElements(input)
+  }
+  private object Empty extends Set {
+    def apply(input: String): Boolean = false
+  }
+  def empty: Set = Empty
 }
