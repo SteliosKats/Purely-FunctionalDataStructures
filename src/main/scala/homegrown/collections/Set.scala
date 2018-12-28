40182c8c480bc2b43610c52a485ad8729c45ff3b
SteliosKats <stelios.katsiadramis@gmail.com>
2018-12-28 04:18:41 +0200

    Updates on Sets (v0.2)

--- src/main/scala/homegrown/collections/Set.scala
+++ src/main/scala/homegrown/collections/Set.scala
@@ -1,19 +1,34 @@
 package homegrown.collections
 
 trait Set extends (String => Boolean) {
-  def add(input: String): Set = element => element == input || this(element)
-  final def remove(input: String): Set = element => input != element && !this(element)
+  def add(input: String): Set
+  def remove(input: String): Set
+  def union(that: Set): Set
+  def intersection(that: Set): Set
+  def diff(that: Set): Set
+  /*
   final def union(that: Set): Set = element => this(element) || that(element)
   final def intersection(that: Set): Set = element => this(element) && that(element)
-  final def diff(that: Set): Set = element => this(element) && !that(element)
+  final def diff(that: Set): Set = element => this(element) && !that(element)*/
 }
 
 object Set {
   private final case class NonEmpty(element: String, otherElements: Set) extends Set {
-    final override def apply(input: String): Boolean = input == element || otherElements(input)
+    def apply(input: String): Boolean = input == element || otherElements(input)
+    final override def add(input: String): Set = if (input == element) this else NonEmpty(input, otherElements.add(element))
+    final override def remove(input: String): Set = if (input == element) otherElements else NonEmpty(element, otherElements.remove(input))
+    final override def union(that: Set): Set = otherElements.union(that.add(element)) //TODO check if otherElements.union(NonEmpty(element, that)) is also true
+    final override def intersection(that: Set): Set = if (that(element)) otherElements.intersection(that).add(element) else otherElements.intersection(that)
+    final override def diff(that: Set): Set = if (that(element)) otherElements.diff(that) else otherElements.diff(that).add(element)
   }
   private object Empty extends Set {
     def apply(input: String): Boolean = false
+    final override def add(input: String): Set = NonEmpty(input, Empty)
+    final override def remove(input: String): Set = this //or Empty
+    final override def union(that: Set): Set = that
+    final override def intersection(that: Set): Set = this //or Empty
+    final override def diff(that: Set): Set = this
   }
+
   def empty: Set = Empty
 }
--- src/test/scala/homegrown/collections/SetSuite.scala
+++ src/test/scala/homegrown/collections/SetSuite.scala
@@ -36,13 +36,13 @@ class TestSuite extends FunSuite with Matchers {
 
   test("remove from an Non Empty set should yield an Empty Set") {
     val first: String = generateRandomStr
-    val addFirstNonEmpty = OldSet.empty.add(first)
+    val addFirstNonEmpty = Set.empty.add(first)
     addFirstNonEmpty(first) shouldBe true
   }
 
   test("remove on a non empty Set should yield a new Set without the element") {
     val element = generateRandomStr
-    val setWithelement = OldSet.empty.add(element)
+    val setWithelement = Set.empty.add(element)
     setWithelement(element) shouldBe true
 
     val setWithoutElement = setWithelement.remove(element)
@@ -53,7 +53,7 @@ class TestSuite extends FunSuite with Matchers {
     val element = generateRandomStr
     val element2 = generateRandomStr
     element should not be element2
-    val setWithelement = OldSet.empty.add(element)
+    val setWithelement = Set.empty.add(element)
     val setWithelement2 = setWithelement.add(element2)
     setWithelement2(element) shouldBe true
 
@@ -61,12 +61,19 @@ class TestSuite extends FunSuite with Matchers {
     setWithoutElement2(element2) shouldBe false
   }
 
+  test("Add/remove combo should ensure that all elements are distinct") {
+    val element = generateRandomStr
+
+    val set = Set.empty.add(element).add(element).remove(element)
+    set(element) shouldBe false
+  }
+
   test("Union on an empty set should yield an empty Set") {
     val element1 = generateRandomStr
     val element2 = generateRandomStr
     element1 should not be element2
-    val emptySet = OldSet.empty
-    val nonEmptySet = emptySet.add(element1).add(element2)
+    val emptySet = Set.empty
+    val nonEmptySet = Set.empty.add(element1).add(element2)
     emptySet.union(nonEmptySet)(element1) shouldBe true
     emptySet.union(nonEmptySet)(element2) shouldBe true
 
@@ -84,8 +91,8 @@ class TestSuite extends FunSuite with Matchers {
     el1 should not be el4
     el2 should not be el3
     el2 should not be el4
-    val nonEmptySet1 = OldSet.empty.add(el1).add(el2)
-    val nonEmptySet2 = OldSet.empty.add(el3).add(el4)
+    val nonEmptySet1 = Set.empty.add(el1).add(el2)
+    val nonEmptySet2 = Set.empty.add(el3).add(el4)
 
     nonEmptySet1.union(nonEmptySet2)(el1) shouldBe true
     nonEmptySet1.union(nonEmptySet2)(el2) shouldBe true
@@ -97,7 +104,7 @@ class TestSuite extends FunSuite with Matchers {
     val element1 = generateRandomStr
     val element2 = generateRandomStr
     element1 should not be element2
-    val emptySet = OldSet.empty
+    val emptySet = Set.empty
     val nonEmptySet = emptySet.add(element1).add(element2)
     emptySet.intersection(nonEmptySet)(element1) shouldBe false
     emptySet.intersection(nonEmptySet)(element2) shouldBe false
@@ -116,8 +123,8 @@ class TestSuite extends FunSuite with Matchers {
     el1 should not be el4
     el2 should not be el3
     el2 should not be el4
-    val nonEmptySet1 = OldSet.empty.add(el1).add(el2)
-    val nonEmptySet2 = OldSet.empty.add(el3).add(el4)
+    val nonEmptySet1 = Set.empty.add(el1).add(el2)
+    val nonEmptySet2 = Set.empty.add(el3).add(el4)
 
     nonEmptySet1.intersection(nonEmptySet2)(el1) shouldBe false
     nonEmptySet1.intersection(nonEmptySet2)(el2) shouldBe false
@@ -129,8 +136,8 @@ class TestSuite extends FunSuite with Matchers {
     val element1 = generateRandomStr
     val element2 = generateRandomStr
     element1 should not be element2
-    val emptySet = OldSet.empty
-    val emptySet2 = OldSet.empty
+    val emptySet = Set.empty
+    val emptySet2 = Set.empty
     emptySet.diff(emptySet2)(element1) shouldBe false
   }
 
@@ -138,7 +145,7 @@ class TestSuite extends FunSuite with Matchers {
     val element1 = generateRandomStr
     val element2 = generateRandomStr
     element1 should not be element2
-    val emptySet = OldSet.empty
+    val emptySet = Set.empty
     val nonEmptySet = emptySet.add(element1).add(element2)
     nonEmptySet.diff(emptySet)(element1) shouldBe true
     nonEmptySet.diff(emptySet)(element2) shouldBe true
@@ -157,8 +164,8 @@ class TestSuite extends FunSuite with Matchers {
     el1 should not be el4
     el2 should not be el3
     el2 should not be el4
-    val nonEmptySet1 = OldSet.empty.add(el1).add(el2)
-    val nonEmptySet2 = OldSet.empty.add(el3).add(el4)
+    val nonEmptySet1 = Set.empty.add(el1).add(el2)
+    val nonEmptySet2 = Set.empty.add(el3).add(el4)
 
     nonEmptySet1.diff(nonEmptySet2)(el1) shouldBe true
     nonEmptySet1.diff(nonEmptySet2)(el2) shouldBe true
