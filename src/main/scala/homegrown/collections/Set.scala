8194e3bbce4e9cb36d197d8a497f4f22da3de7d7
SteliosKats <stelios.katsiadramis@gmail.com>
2019-01-02 22:36:39 +0200

    Refatored remove,add and apply functions to be implemented on trait for all Set object instances

--- src/main/scala/homegrown/collections/Set.scala
+++ src/main/scala/homegrown/collections/Set.scala
@@ -1,8 +1,28 @@
 package homegrown.collections
 
 trait Set extends (String => Boolean) {
-  def add(input: String): Set
-  def remove(input: String): Set
+  import Set._
+  final override def apply(input: String): Boolean = {
+    var result = false
+    foreach {
+      current => result = result || current == input
+    }
+    result
+  }
+  final def add(input: String): Set = {
+    var result = NonEmpty(input, empty)
+    foreach {
+      current => if (current != input) result = NonEmpty(current, result)
+    }
+    result
+  }
+  final def remove(input: String): Set = {
+    var result = Set.empty
+    foreach {
+      current => if (input != current) result = NonEmpty(current, result)
+    }
+    result
+  }
   def union(that: Set): Set
   def intersection(that: Set): Set
   def diff(that: Set): Set
@@ -26,9 +46,6 @@ object Set {
     result
   }
   private final case class NonEmpty(element: String, otherElements: Set) extends Set {
-    def apply(input: String): Boolean = input == element || otherElements(input)
-    final override def add(input: String): Set = if (input == element) this else NonEmpty(input, otherElements.add(element))
-    final override def remove(input: String): Set = if (input == element) otherElements else NonEmpty(element, otherElements.remove(input))
     final override def union(that: Set): Set = otherElements.union(that.add(element)) //TODO check if otherElements.union(NonEmpty(element, that)) is also true
     final override def intersection(that: Set): Set = if (that(element)) otherElements.intersection(that).add(element) else otherElements.intersection(that)
     final override def diff(that: Set): Set = if (that(element)) otherElements.diff(that) else otherElements.diff(that).add(element)
@@ -43,9 +60,6 @@ object Set {
 
   }
   private object Empty extends Set {
-    def apply(input: String): Boolean = false
-    final override def add(input: String): Set = NonEmpty(input, Empty)
-    final override def remove(input: String): Set = this //or Empty
     final override def union(that: Set): Set = that
     final override def intersection(that: Set): Set = this //or Empty
     final override def diff(that: Set): Set = this
